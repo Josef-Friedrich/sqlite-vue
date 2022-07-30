2ab527a71f824518456bf32f6c26568819e660c9
Josef Friedrich <josef@friedrich.rocks>
2022-07-30 19:33:07 +0200

    Add sample code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import initSqlJs from 'sql.js'
+
+// Create a database
+</script>
+
+<script lang="ts">
+export default {
+  async mounted () {
+    const SQL = await initSqlJs({
+      // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+      // You can omit locateFile completely when running in node
+      // `node_modules/sql.js/dist/${file}`
+      // `https://sql.js.org/dist/${file}`
+      locateFile: file => `https://sql.js.org/dist/${file}`
+    })
+    const db = new SQL.Database()
+
+    // Run a query without reading the results
+    db.run('CREATE TABLE test (col1, col2);')
+    // Insert two rows: (1,111) and (2,222)
+    db.run('INSERT INTO test VALUES (?,?), (?,?)', [1, 111, 2, 222])
+
+    // Prepare a statement
+    const stmt = db.prepare(
+      'SELECT * FROM test WHERE col1 BETWEEN $start AND $end'
+    )
+    stmt.getAsObject({ $start: 1, $end: 1 }) // {col1:1, col2:111}
+
+    // Bind new values
+    stmt.bind({ $start: 1, $end: 2 })
+    while (stmt.step()) {
+      //
+      const row = stmt.getAsObject()
+      console.log('Here is a row: ' + JSON.stringify(row))
+    }
+  }
+}
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
