5505c8b3843ed29db7f85ade399bd3472815099d
Josef Friedrich <josef@friedrich.rocks>
2022-08-01 22:56:15 +0200

    Refactor router

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import QueryView from '@/views/QueryView.vue'
 import DatabaseView from '@/views/DatabaseView.vue'
+import AboutView from '@/views/AboutView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -17,10 +19,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     }
   ]
 })
