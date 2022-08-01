fd142c3d1929f6b7000d8b02e8c0d952f8ee910d
Josef Friedrich <josef@friedrich.rocks>
2022-08-01 22:52:50 +0200

    Rename home to query

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-start">
-        <RouterLink class="navbar-item" to="/">Home</RouterLink>
+        <RouterLink class="navbar-item" to="/">Query</RouterLink>
         <RouterLink class="navbar-item" to="/database">Database</RouterLink>
         <RouterLink class="navbar-item" to="/about">About</RouterLink>
       </div>
