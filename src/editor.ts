b34ec3621a0ea5f7163d44e4af31d329a459a9dd
Josef Friedrich <josef@friedrich.rocks>
2022-08-02 22:52:37 +0200

    Try web workers

--- src/editor.ts
+++ src/editor.ts
@@ -1,5 +1,35 @@
 import * as monaco from 'monaco-editor'
 
+
+// https://github.com/vitejs/vite/discussions/1791
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+
+// Since packaging is done by you, you need
+// to instruct the editor how you named the
+// bundles that contain the web workers.
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId: string, label: string) {
+    console.log(label)
+    if (label === 'json') {
+      return new jsonWorker()
+    }
+    if (label === 'css') {
+      return new cssWorker()
+    }
+    if (label === 'html') {
+      return new htmlWorker()
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker()
+    }
+    return new editorWorker()
+  }
+}
+
 // https://github.com/SadeghPM/sql-vscode-snipptes/blob/master/snippets/snippets.json
 monaco.languages.registerCompletionItemProvider('sql', {
   provideCompletionItems: (model, position, context, token) => {
@@ -28,7 +58,9 @@ monaco.languages.registerCompletionItemProvider('sql', {
 
 export let editor: monaco.editor.IStandaloneCodeEditor | null
 
-export function create(element: HTMLElement): monaco.editor.IStandaloneCodeEditor {
+export function create (
+  element: HTMLElement
+): monaco.editor.IStandaloneCodeEditor {
   editor = monaco.editor.create(element, {
     language: 'sql',
     theme: 'vs-dark',
