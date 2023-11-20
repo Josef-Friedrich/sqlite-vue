08b4cccaf686689f39d89cc4c39e7b2a7127f473
Josef Friedrich <josef@friedrich.rocks>
2023-11-20 22:37:12 +0100

    Add some snippets

--- src/editor.ts
+++ src/editor.ts
@@ -10,6 +10,36 @@ self.MonacoEnvironment = {
   }
 }
 
+interface Snippet {
+  label: string
+  documentation: string
+  insertText: string
+}
+
+// https://microsoft.github.io/monaco-editor/playground.html?source=v0.44.0#example-extending-language-services-completion-provider-example
+const snippets: Snippet[] = [
+  {
+    label: 'SELECT * FROM table',
+    documentation: 'select',
+    insertText: 'SELECT ${1:*} FROM ${2:table}'
+  },
+  {
+    label: 'FROM',
+    documentation: 'select',
+    insertText: 'FROM'
+  },
+  {
+    label: 'ORDER BY',
+    documentation: 'SELECT * FROM table ORDER BY attribute ASC',
+    insertText: 'ORDER BY ${1:attribute}'
+  },
+  {
+    label: 'WHERE',
+    documentation: 'SELECT * FROM table WHERE <bedingung>',
+    insertText: 'WHERE'
+  }
+]
+
 // https://github.com/SadeghPM/sql-vscode-snipptes/blob/master/snippets/snippets.json
 monaco.languages.registerCompletionItemProvider('sql', {
   provideCompletionItems: (model, position) => {
@@ -20,18 +50,23 @@ monaco.languages.registerCompletionItemProvider('sql', {
       startColumn: word.startColumn,
       endColumn: word.endColumn
     }
+
+    const suggestions = []
+
+    for (const snippet of snippets) {
+      suggestions.push({
+        label: snippet.label,
+        kind: monaco.languages.CompletionItemKind.Snippet,
+        documentation: snippet.documentation,
+        insertText: snippet.insertText,
+        insertTextRules:
+          monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
+        range
+      })
+    }
+
     return {
-      suggestions: [
-        {
-          label: 'SELECT FROM',
-          kind: monaco.languages.CompletionItemKind.Snippet,
-          documentation: 'select',
-          insertText: 'SELECT ${1:*} FROM ${2:table}',
-          insertTextRules:
-            monaco.languages.CompletionItemInsertTextRule.InsertAsSnippet,
-          range
-        }
-      ]
+      suggestions
     }
   }
 })
